9ea37307458cc90b6ea1a6301ecf0e6b7bf5a0cb
B. P. Hon <benji.h.peng@gmail.com>
2024-10-15 22:04:41 -0400

    add footer

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ShineBorder } from "./ui/magicui/sine-border";
 import SparklesText from "./ui/magicui/sparkles-text";
 import FlickeringGrid from "@/components/ui/magicui/flickering-grid";
 import { AnimatedText } from "./hero/animated-text";
 
 export const HeroSection: React.FC = () => {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  useEffect(() => {
+    const isDarkMode = document.documentElement.classList.contains("dark");
+    setTheme(isDarkMode ? "dark" : "light");
+
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.attributeName === "class") {
+          const isDarkMode =
+            document.documentElement.classList.contains("dark");
+          setTheme(isDarkMode ? "dark" : "light");
+        }
+      });
+    });
+
+    observer.observe(document.documentElement, { attributes: true });
+
+    return () => observer.disconnect();
+  }, []);
+
+  const gridColor =
+    theme === "light" ? "rgb(167, 208, 148)" : "rgb(10, 99, 15)";
+
   return (
     <ShineBorder
       className="h-[60vh] p-0 relative flex w-full flex-col items-center justify-center rounded-lg border bg-background md:shadow-xl mb-8 overflow-hidden"
@@ -15,9 +39,9 @@ export const HeroSection: React.FC = () => {
           className="w-full h-full"
           squareSize={4}
           gridGap={6}
-          color="#bababa"
           maxOpacity={0.5}
           flickerChance={0.1}
+          color={gridColor}
         />
       </div>
       <div className="relative z-10 flex flex-col items-center text-center max-w-4xl p-8">
